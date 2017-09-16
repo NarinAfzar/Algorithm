a40d822c8dbdbbf3d15f279ab586f9bb22befaa0
Bahman Masarratbakhsh <atropatnet@yahoo.com>
2017-09-15 23:34:12 -0700

    Skeleton

---
+++ UnitTest_TwoSum/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_TwoSum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_TwoSum")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cbbf63f2-9cbf-4599-b162-008d025c9761")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest_TwoSum/TwoSum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_TwoSum
+{
+    [TestClass]
+    public class TwoSum
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
